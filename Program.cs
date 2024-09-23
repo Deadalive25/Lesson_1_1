879d9781f7ad0fd37ea2e0a662b9da01bd0b706b
Deadalive25 <alekseevdi1990@gmail.com>
2024-09-23 17:37:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите фамилию:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Введите имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите отчество:");
+            string middle_name = Console.ReadLine();
+            Console.WriteLine(surname + " " + name + " " + middle_name);
+        }
+    }
+}
